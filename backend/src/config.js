5ba4ad974ca77da1e33945f084e558c51c2dbc92
Kisnierius <84340591+Kisnierius@users.noreply.github.com>
2021-12-06 21:20:23 +0200

    First commit backend
    index.js plus router structure

---
+++ backend/src/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+  portCon: process.env.PORT || 8080,
+  dbConfig: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
+};
---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const carsRouter = require('./routes/cars');
+const ownersRouter = require('./routes/owners');
+const repairsRouter = require('./routes/repairs');
+const partsRouter = require('./routes/parts');
+
+const { portCon } = require('./config');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(carsRouter);
+app.use(ownersRouter);
+app.use(repairsRouter);
+app.use(partsRouter);
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Server is running' });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send({ error: 'Page not found' });
+});
+
+app.listen(portCon, () => console.log(`Listening on port ${portCon}`));
