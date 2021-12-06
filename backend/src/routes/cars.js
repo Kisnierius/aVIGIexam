4438ba2c4a6a06bec205d1c1cae9253db7727284
Kisnierius <84340591+Kisnierius@users.noreply.github.com>
2021-12-06 21:45:59 +0200

    Updated cars router
    v1 of cars router

--- backend/src/routes/cars.js
+++ backend/src/routes/cars.js
@@ -1,1 +1,63 @@
-test
+const express = require('express');
+const carsEntity = require('../../database/cars');
+
+const router = express.Router();
+
+const handleGetcars = async (req, res) => {
+    try {
+        const cars = await carsEntity.getcars();
+        res.send(cars);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const handleGetcar = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(id);
+        const car = await carsEntity.getcar(id);
+        res.send(car);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const handleCreatecar = async (req, res) => {
+    try {
+        const newcar = req.body;
+        const newcarCreated = await carsEntity.createcar(newcar);
+        res.send(newcarCreated);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const handleDeletecar = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const car = await carsEntity.deletecar(id);
+        res.send(car);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const handleUpdateOwnerId = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { owner_Id } = req.params;
+        const car = await carsEntity.UpdateOwnerId(id, owner_Id);
+        res.send(car);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+router.get('/cars', handleGetcars);
+router.get('/cars/:id', handleGetcar);
+router.delete('/cars/:id', handleDeletecar);
+router.post('/cars/:id', handleUpdateOwnerId);
+router.post('/cars', handleCreatecar);
+
+module.exports = router;
