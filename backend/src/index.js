93e8014f47d414c685842da46345a5bc6b4a08d1
Kisnierius <84340591+Kisnierius@users.noreply.github.com>
2021-12-08 16:43:28 +0200

    new router imported to index

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const carsRouter = require('./routes/cars');
 const UsersRouter = require('./routes/users');
-// const ownersRouter = require('./routes/owners');
+const ownersRouter = require('./routes/owners');
 // const repairsRouter = require('./routes/repairs');
 // const partsRouter = require('./routes/parts');
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(carsRouter);
-// app.use(ownersRouter);
+app.use(ownersRouter);
 // app.use(repairsRouter);
 // app.use(partsRouter);
 app.use(UsersRouter);
